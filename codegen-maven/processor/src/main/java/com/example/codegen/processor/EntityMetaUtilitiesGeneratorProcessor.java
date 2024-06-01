c9a791613fb7baadb125cc4a0626b6e2bd9ebb37
cproinger <christian.proinger@gmail.com>
2024-06-01 11:31:26 +0200

    Annotation Processor generates ALL_SINGULAR constant

--- codegen-maven/processor/src/main/java/com/example/codegen/processor/EntityMetaUtilitiesGeneratorProcessor.java
+++ codegen-maven/processor/src/main/java/com/example/codegen/processor/EntityMetaUtilitiesGeneratorProcessor.java
@@ -13,12 +13,12 @@ import javax.lang.model.type.TypeVisitor;
 import javax.lang.model.util.ElementFilter;
 import javax.persistence.metamodel.PluralAttribute;
 import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
 import javax.tools.Diagnostic;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @SupportedAnnotationTypes({"javax.persistence.metamodel.StaticMetamodel"
 //        "javax.persistence.Entity", "javax.persistence.MappedSuperclass", "javax.persistence.Embeddable"
@@ -52,11 +52,15 @@ public class EntityMetaUtilitiesGeneratorProcessor extends javax.annotation.proc
                                 getClass().getSimpleName() + " processing " + tated);
 
                 if (tated.getKind().isClass()) {
+                    // Attempt to access Class object for TypeMirror com.example.codgen.app.AbstractSampleEntity
+//                    var entityClass = tated.getAnnotation(StaticMetamodel.class).value();
                     TypeElement typeElement = (TypeElement) tated;
                     try {
 //                        typeElement.asType().accept(new TypeVisitor<Object, Object>() {
 //                        })
-                        String genTypeSimpleName = typeElement.getSimpleName().toString() + "MM";
+                        String hibernateStaticMetamodelType = typeElement.getSimpleName()
+                                .toString();
+                        String genTypeSimpleName = hibernateStaticMetamodelType + "MM";
                         String packageName = extractPackageName(typeElement);
                         String genTypeFqn = packageName + "." + genTypeSimpleName;
                         var javaFile = processingEnv.getFiler().createSourceFile(genTypeFqn);
@@ -67,6 +71,7 @@ public class EntityMetaUtilitiesGeneratorProcessor extends javax.annotation.proc
                             var imports = new Imports();
                             imports.add("javax.annotation.processing.Generated");
                             imports.add("java.util.function.Function");
+                            imports.add("java.util.List");
                             imports.add(MMGetter.class.getName());
 
                             imports.generate(pw);
@@ -77,47 +82,24 @@ public class EntityMetaUtilitiesGeneratorProcessor extends javax.annotation.proc
                             pw.println("public class %s {".formatted(genTypeSimpleName));
                             pw.println();
 
-                            var attributes = ElementFilter.fieldsIn(typeElement.getEnclosedElements())
-                                    .stream()
-                                    .filter(e -> e.asType()
-                                            .toString()
-                                            .startsWith("javax.persistence.metamodel."))
-//                                    .map(e -> StaticMetamodelAttribute.of(context, e))
-                                    .filter(e -> !e.getSimpleName().toString().equals("id"))
-                                    .sorted((e1, e2) -> e1.getSimpleName().toString().compareTo(e2.getSimpleName().toString()))
-                                    .toList();
-                            pw.println("// " + attributes + ", " + attributes.stream().map(e -> e.asType()).toList());
-                            for (VariableElement attr : attributes) {
-                                TypeMirror attrType = attr.asType();
-                                DeclaredType declaredType = (DeclaredType) attrType;
-//                                processingEnv.getTypeUtils().getDeclaredType(attr., attrType);
-                                processingEnv.getMessager()
-                                        .printMessage(Diagnostic.Kind.NOTE,
-                                                getClass().getSimpleName() + " processing " + tated + ": " + declaredType);
+                            var generatedAttributes = new ArrayList<String>();
+                            generateAttributes(tated, typeElement, pw, generatedAttributes);
 
-                                // TODO find a better way to do this
-                                if (!declaredType.toString().substring(0, declaredType.toString().indexOf('<')).equals(SingularAttribute.class.getName())) {
-                                    continue;
-                                }
-
-                                pw.println(("""
-                                            public static final MMGetter<%1$s, %2$s> %4$s
-                                                = new MMGetter<>(\"%3$s\", %1$s.class, %2$s.class , %1$s::get%6$s);
-                                        """).formatted(
-                                        declaredType.getTypeArguments().get(0),
-                                        declaredType.getTypeArguments().get(1),
-                                        attr.getSimpleName().toString(),
-                                        "GET_" + attr.getSimpleName().toString(),
-//                                        attr.getSimpleName().toString()
-                                        declaredType.getTypeArguments().get(1),
-                                        Character.toUpperCase(attr.getSimpleName().toString().charAt(0)) + attr.getSimpleName().toString().substring(1)
-                                ));
-                            }
+                            pw.println();
+                            pw.println("""
+                                    %1$spublic static final List<MMGetter<%2$s,?>> ALL_SINGULAR = List.of(
+                                    %1$s    %3$s
+                                    %1$s); 
+                                    """.formatted(
+                                            "    ",
+                                            hibernateStaticMetamodelType.substring(0,hibernateStaticMetamodelType.length() - 1),
+                                    generatedAttributes.stream()
+                                            .collect(Collectors.joining(",\n%s".formatted("        ")))));
 
                             pw.println();
                             pw.println("}");
                             // needed?
-                            pw.flush();
+//                            pw.flush();
                         }
                     } catch (IOException e) {
                         throw new RuntimeException(e);
@@ -127,6 +109,48 @@ public class EntityMetaUtilitiesGeneratorProcessor extends javax.annotation.proc
         }
 
         return false;
+    }
+
+    private void generateAttributes(Element tated, TypeElement typeElement, PrintWriter pw, List<String> generatedConstants) {
+        var attributes = ElementFilter.fieldsIn(typeElement.getEnclosedElements())
+                .stream()
+                .filter(e -> e.asType().toString().startsWith("javax.persistence.metamodel."))
+//                                    .map(e -> StaticMetamodelAttribute.of(context, e))
+                .filter(e -> !e.getSimpleName().toString().equals("id"))
+                .sorted((e1, e2) -> e1.getSimpleName()
+                        .toString()
+                        .compareTo(e2.getSimpleName().toString()))
+                .toList();
+        pw.println("// " + attributes + ", " + attributes.stream().map(e -> e.asType()).toList());
+        for (VariableElement attr : attributes) {
+            TypeMirror attrType = attr.asType();
+            DeclaredType declaredType = (DeclaredType) attrType;
+//                                processingEnv.getTypeUtils().getDeclaredType(attr., attrType);
+            processingEnv.getMessager()
+                    .printMessage(Diagnostic.Kind.NOTE,
+                            getClass().getSimpleName() + " processing " + tated + ": " + declaredType);
+
+            // TODO find a better way to do this
+            if (!declaredType.toString()
+                    .substring(0, declaredType.toString().indexOf('<'))
+                    .equals(SingularAttribute.class.getName())) {
+                continue;
+            }
+
+            String mmGetterConstant = "GET_" + attr.getSimpleName().toString();
+            generatedConstants.add(mmGetterConstant);
+            pw.println(("""
+                        public static final MMGetter<%1$s, %2$s> %4$s
+                            = new MMGetter<>(\"%3$s\", %1$s.class, %2$s.class , %1$s::get%6$s);
+                    """).formatted(declaredType.getTypeArguments().get(0),
+                    declaredType.getTypeArguments().get(1),
+                    attr.getSimpleName().toString(),
+                    mmGetterConstant,
+                    declaredType.getTypeArguments().get(1),
+                    Character.toUpperCase(attr.getSimpleName()
+                            .toString()
+                            .charAt(0)) + attr.getSimpleName().toString().substring(1)));
+        }
     }
 
     private String extractPackageName(TypeElement typeElement) {
